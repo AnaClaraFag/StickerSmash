2bf3931c1e12db35b0d125b6e89f597531c0936d
AnaClaraFag <clarafag@gmail.com>
2025-05-14 12:38:53 -0300

    add screenshot

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,13 +6,17 @@ import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import { useRef, useState } from "react";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const imageRef = useRef<View>(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
@@ -47,15 +51,31 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    try{
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1});
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if(localUri){
+          alert("Image saved to gallery");
+        }
+      }catch(e){
+        console.log(e);
+      }
   };
+
+  if(status === null){
+    requestPermission();
+  }
 
   return (
     <GestureHandlerRootView style = { styles.container}>
       <View style = {styles.container}>
       <View style={styles.imageContainer}>
+        <View ref = { imageRef } collapsable={false} >
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+         </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
